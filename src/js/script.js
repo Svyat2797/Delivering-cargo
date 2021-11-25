d187b5d89e44f3bcdc1e285c2a8e8ae16c7e55c5
Slava <example@gmail.com>
2021-11-25 22:23:07 +0300

    Last block

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const slider = tns({
+    container: '.review__wrapper',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    nav: false,
+    controls: false,
+    speed: 1000,
+
+  });
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next'); 
+  });
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev'); 
+  }); 
+  
